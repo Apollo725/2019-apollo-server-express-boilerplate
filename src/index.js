ad05e05cf7496e0693dd211eda0dac04f2f541c1
apollo725 <superapollo0725@gmail.com>
2019-06-27 16:05:25 +0200

    add mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import mongoose from 'mongoose';
+
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
 
@@ -16,3 +18,23 @@ server.applyMiddleware({ app }); // app is from an existing express app
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
+
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({
+    // These will be defined for both new or existing servers
+    typeDefs,
+    resolvers,
+  });
+
+  server.applyMiddleware({ app }); // app is from an existing express app
+
+  await mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  )
+}
+
+startServer();
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,9 +1,16 @@
-import { gql } from 'apollo-server-express';
+import { Item } from './models/Item';
 
 export const resolvers = {
   Query: {
     hello: () => {
       return "hello";
+    }
+  },
+  Mutation: {
+    createItem: async (_, { name }) => {
+      const item = new Item({ name });
+      await item.save();
+      return item;
     }
   }
 };
