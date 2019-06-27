707197f6811a1102c57701ff35f4141631722010
apollo725 <superapollo0725@gmail.com>
2019-06-27 15:43:59 +0200

    complete setup

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import express from 'express';
+// import { typeDefs, resolvers } from './schema';
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "hello";
+    }
+  }
+};
+
+const server = new ApolloServer({
+  // These will be defined for both new or existing servers
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app }); // app is from an existing express app
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
